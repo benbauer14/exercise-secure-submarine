67da0cab1f9b1e84015f28595c72f71ea27555d4
Ben Bauer <ben.bauer1@gmail.com>
2021-04-13 16:44:49 -0500

    temp get and push routes working

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+const encryptLib = require('../modules/encryption');
+const userStrategy = require('../strategies/user.strategy');
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get('/temp',rejectUnauthenticated, (req, res) => {
   // GET route code here
+  console.log(req.user.username)
+  if(req.user.username){
+    res.send('secrets secrets hurt someone')
+  }else{
+    res.sendStatus(503)
+  }
 });
 
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
+router.post('/temp',rejectUnauthenticated, (req, res) => {
   // POST route code here
+  if(req.user.username){
+    res.send('secrets secrets hurt someone')
+  }else{
+    res.sendStatus(503)
+  }
 });
 
 module.exports = router;

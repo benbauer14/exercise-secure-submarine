771271e29f6a679d1bdadb5b7dc03a864b0d92d9
Ben Bauer <ben.bauer1@gmail.com>
2021-04-13 16:59:38 -0500

    added comments to template router

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+//needed for auth
 const {
   rejectUnauthenticated,
 } = require('../modules/authentication-middleware');
@@ -10,13 +11,13 @@ const userStrategy = require('../strategies/user.strategy');
 /**
  * GET route template
  */
-router.get('/temp',rejectUnauthenticated, (req, res) => {
+router.get('/temp',rejectUnauthenticated, (req, res) => { //added rejectUnauthenticated to this line to force auth
   // GET route code here
-  console.log(req.user.username)
-  if(req.user.username){
+  console.log(req.user.username) //logs the user name of logged in user
+  if(req.user.username === 'ben'){
     res.send('secrets secrets hurt someone') //when logged in on local3000, when visiting local5000/api/temp/temp
   }else{
-    res.sendStatus(503)
+    res.send('some secrets') // //if username is not ben but exists, they get some secrets. If not logged in they get forbidden.
   }
 });
 
@@ -25,10 +26,10 @@ router.get('/temp',rejectUnauthenticated, (req, res) => {
  */
 router.post('/temp',rejectUnauthenticated, (req, res) => {
   // POST route code here
-  if(req.user.username){
+  if(req.user.username === 'ben'){
     res.send('secrets secrets hurt someone')
   }else{
-    res.sendStatus(503)
+    res.send('some secrets') //
   }
 });
 
